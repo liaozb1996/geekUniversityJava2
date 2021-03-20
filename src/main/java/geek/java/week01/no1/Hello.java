2af6ff4dcc805813dbfa77f628f608399d60f53a
廖子博 <liaozibo@qq.com>
2021-03-20 18:32:26 +0800

    完成 week01-no1

---
+++ src/main/java/geek/java/week01/no1/Hello.java
@@ -0,0 +1,19 @@
+package geek.java.week01.no1;
+
+/**
+ * @author: liaozibo
+ * @date: 2021/3/20
+ */
+public class Hello {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        if (c < 10) {
+            c++;
+        }
+        for (int i = 0; i < 5; i++) {
+            c--;
+        }
+    }
+}
